c7e22e229dadf3957c7ff406c0e09fdcda3e9c9b
exghost <robertlucas198@gmail.com>
2021-07-06 07:09:21 -0400

    fix for references

--- src/auth/components/RegisterForm.js
+++ src/auth/components/RegisterForm.js
@@ -1,18 +1,19 @@
 import './RegisterForm.css';
 
 const RegisterForm = () => {
+
     return (
         <div>
             <form>
                 <div className="form-grid-container">
                     <div className="form-header">Registration Form</div>
-                    <label for="userEmail" className="form-grid-item form-label">Email</label>
+                    <label htmlFor="userEmail" className="form-grid-item form-label">Email</label>
                     <input type="email" id="userEmail" className="form-grid-item form-field" />
-                    <label for="userPassword" className="form-grid-item form-label">Password</label>
+                    <label htmlFor="userPassword" className="form-grid-item form-label">Password</label>
                     <input type="password" id="userPassword" className="form-grid-item form-field" />
-                    <label for="userPasswordConfirm" className="form-grid-item form-label">Confirm Password</label>
+                    <label htmlFor="userPasswordConfirm" className="form-grid-item form-label">Confirm Password</label>
                     <input type="password" id="userPasswordConfirm" className="form-grid-item form-field" />
-                    <button type="submit" className="form-submit">Register</button>
+                    <button type="submit" className="form-submit" value="submit">Register</button>
                 </div>
             </form>
         </div>
