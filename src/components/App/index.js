c2f46c288487496df6cd1abbc9c8d76ee3080261
exghost <robertlucas198@gmail.com>
2021-07-13 08:45:25 -0400

    add logged in navbar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import NavBar from 'components/NavBar';
 import RegistrationForm from 'components/auth/RegistrationForm';
 import Login from 'components/auth/Login';
+import Logout from 'components/auth/Logout';
 import AddBook from 'components/books/AddBook';
 import BookLibrary from 'components/books/BookLibrary';
 
 function App() {
+    const loggedIn = useSelector(state => state.user.loggedIn);
+
   return (
     <div>
         <Router>
-            <NavBar>
-                <Link className="nav-link" to="/login">Login</Link>
-                <Link className="nav-link" to="/register">Register</Link>
-            </NavBar>
+            { !loggedIn && 
+                <NavBar>
+                    <Link className="nav-link" to="/login">Login</Link>
+                    <Link className="nav-link" to="/register">Register</Link>
+                </NavBar>
+            }
+            {
+                loggedIn &&
+                <NavBar>
+                    <Link className="nav-link" to="/books">Library</Link>
+                    <Link className="nav-link" to="/books/new">New Book</Link>
+                    <Link className="nav-link" to="/logout"> Logout</Link>
+                </NavBar>
+            }
             <Switch>
                 <Route exact path="/">
                     <RegistrationForm />
@@ -28,8 +42,11 @@ function App() {
                 <Route exact path="/books">
                     <BookLibrary />
                 </Route>
-                <Route exact path="/book/add">
+                <Route exact path="/books/add">
                     <AddBook />
+                </Route>
+                <Route exact path="/logout">
+                    <Logout />
                 </Route>
             </Switch>
         </Router>
