2ce23e34b6a8ef7a281c18c2201b0c8093133a7b
exghost <robertlucas198@gmail.com>
2021-07-15 23:44:18 -0400

    cleanup unused api func

--- src/api/bookReader/authors.js
+++ src/api/bookReader/authors.js
@@ -1,30 +1,6 @@
 import bookReaderBase from 'api/bookReader/base';
 
 
-export const authorCount = async () => {
-    let response;
-    
-    try {
-        response = await bookReaderBase({
-            method: 'post',
-            data: {
-                query: `
-                    query {
-                        authorCount
-                    }
-                `
-            }
-        })
-    } catch (err) {
-        throw new Error('Unable to get author count');
-    }
-
-    if(response.data?.error) throw new Error('Unable to get author count');
-
-    return response.data?.data?.authorCount;
-}
-
-
 export const fetchAllAuthors = async () => {
     let response;
     
